8d0093bf517643f5b1e353d45bbdc26f5c35da50
vishnu prakash <vishnuvictorio@gmail.com>
2023-02-23 16:58:06 +0530

    Modified SelecctAll component.

--- src/components/SelectAll.js
+++ src/components/SelectAll.js
@@ -2,23 +2,30 @@ import { React } from 'react';
 import { Checkbox } from '@mui/material';
 import TodoManager from '../services/TodoManager';
 
-const SelectAll = (context) => {
-	const { state, setState } = context;
+const checkBox = (
+	context, setState, state
+) =>
+	<Checkbox
+		className="checkbox"
+		checked={ TodoManager.isChecked({ ...context }) }
+		onClick={ ({ target: { checked }}) => setState({
+			...state,
+			todos: TodoManager
+				.toggleAll({ ...context, checked }),
+		}) }
+		variant="contained"
+		size="medium"
+	/>;
 
-	return (
-		<Checkbox
-			className="checkbox"
-			checked={ TodoManager.isChecked({ ...context }) }
-			onClick={ ({ target: { checked }}) =>
-				setState({
-					...state,
-					todos: TodoManager
-						.toggleAll({ ...context, checked }),
-				}) }
-			variant="contained"
-			size="medium"
-		/>
-	);
+const SelectAll = (context) => {
+	const { state, setState, state: { todos }} = context;
+	const isTicked = todos.some((todo) => todo.isCompleted);
+
+	return isTicked
+	&&	checkBox(
+		context, setState, state
+	)
+	;
 };
 
 export default SelectAll;
